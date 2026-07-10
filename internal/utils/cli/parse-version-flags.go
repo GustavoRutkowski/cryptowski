d6e55b0f0037a92bbd7ab40846d8af8a5c939b85
GustavoRutkowski <rutkowskigustavo@gmail.com>
2026-07-10 15:19:27 -0300

    Link V2 struct in parse-version-flags.go

--- internal/utils/cli/parse-version-flags.go
+++ internal/utils/cli/parse-version-flags.go
@@ -4,11 +4,12 @@ import (
 	"flag"
 	"github.com/GustavoRutkowski/cryptowski/internal/cryptowski"
 	"github.com/GustavoRutkowski/cryptowski/internal/cryptowski/v1"
+	"github.com/GustavoRutkowski/cryptowski/internal/cryptowski/v2"
 )
 
 func ParseVersionFlags() cryptowski.ICrypto {
 	isV1 := flag.Bool("v1", false, "Use version 1")
-	// isV2 := flag.Bool("v2", false, "Use version 2")
+	isV2 := flag.Bool("v2", false, "Use version 2")
 	// ...
 	flag.Parse()
 	
@@ -16,8 +17,10 @@ func ParseVersionFlags() cryptowski.ICrypto {
 	switch {
 	case *isV1:
 		return v1.Crypto{}
+	case *isV2:
+		return v2.Crypto{}
 	default:
-		latest := v1.Crypto{}
+		latest := v2.Crypto{}
 		return latest
 	}
 }
